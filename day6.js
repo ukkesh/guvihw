c46e4433db1ab29acb2d0f7c94c55059395691d0
ukkesh <81634856+ukkesh@users.noreply.github.com>
2023-07-04 19:21:59 +0530

    day 6

---
+++ day6.js
@@ -0,0 +1,11 @@
+3 . 
+
+class person{
+  constructor(name , gender, age){
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+  }
+}
+var a = new person("RAM", "MALE" , "30");
+var b = new person("SITHA" , "FEMALE" , "30");
